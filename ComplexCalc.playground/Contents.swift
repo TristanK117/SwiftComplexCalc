68caaf491e10dc894305408f31baee1ed81ab6a7
TristanK117 <81729665+TristanK117@users.noreply.github.com>
2025-04-09 21:36:43 -0700

    partial

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -28,6 +28,53 @@ print("Welcome back to the UW Calculator")
 //: IMPORTANT: If any tests are commented out, you will be graded a zero (0)! You should never be in the habit of eliminating tests to make the code pass.
 //:
 class Calculator {
+    func add(lhs : Int, rhs : Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs : Int, rhs : Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs : Int, rhs : Int) -> Int  {
+        return lhs * rhs
+    }
+    
+    func divide(lhs : Int, rhs : Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func mathOp(lhs : Int, rhs : Int, op : (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func add(_ list: [Int]) -> Int {
+        var sum = 0
+        for i in list {
+            sum += i
+        }
+        return sum
+    }
+    
+    func multiply(_ list: [Int]) -> Int {
+        var sum = 0
+        for i in list {
+            sum *= i
+        }
+        return sum
+    }
+    
+    func count(_ list: [Int]) -> Int {
+        return list.count
+    }
+    
+    func avg(_ list: [Int]) -> Double {
+        return Double(add(list)) / Double(count(list))
+    }
+    
+    func mathOp(_ list: [Int], op: ([Int]) -> Int) -> Int {
+        return op(list)
+    }
 }
 
 //: Don't change the name of this object (`calc`); it's used in all the tests.
@@ -52,7 +99,7 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
